7a840f216f352e01d4aa51e64925e5af63a21202
LORDxisserious <91208047+LORDxisserious@users.noreply.github.com>
2022-04-06 23:11:26 +0630

    finished comment template

---
+++ components/container/container.js
@@ -0,0 +1,8 @@
+import Comment from "../util/comment";
+export default function Container() {
+  return (
+    <div className="comments-container justify-contene-center align-items-center py-3">
+      <Comment />
+    </div>
+  );
+}
---
+++ components/util/comment.js
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import { IconContext } from "react-icons";
+import { FaPlus, FaMinus, FaReply } from "react-icons/fa";
+
+export default function Comment() {
+  return (
+    <div className="w-100 row comment p-4">
+      <div className="d-flex justify-content-center align-items-start p-2 pt-0 col-1">
+        <div className="d-flex flex-column justify-content-between vote-container">
+          <IconContext.Provider value={{ size: 13, className: "vote-icon" }}>
+            <FaPlus />
+
+            <div className="my-2 vote-count">12</div>
+
+            <FaMinus />
+          </IconContext.Provider>
+        </div>
+      </div>
+      <div className="col-11 ps-3">
+        <div className="d-flex justify-content-between">
+          <div className="d-flex justify-content-center align-items-center">
+            <Image
+              src="/src/images/avatars/image-amyrobson.png"
+              width="30"
+              height="30"
+            />
+
+            <div className="me-1 ms-3 user-name">Zar Ne</div>
+            <div className="mx-2 posted-date">23 week ago</div>
+          </div>
+          <div className="rp-btn">
+            <IconContext.Provider
+              value={{ className: "rp-icon me-1", size: 15 }}
+            >
+              <FaReply /> Reply
+            </IconContext.Provider>
+          </div>
+        </div>
+
+        <div className="pt-2 comment-text ">
+          Lorem ipsumsdafsdffdjjfiru juirhjguiiiiiiiiiiii iii iiiiiiiiiiiiiiiiii
+          iiiiiiiiiiii iiiiis8rrrrrrrrrrrri opk
+        </div>
+      </div>
+    </div>
+  );
+}
